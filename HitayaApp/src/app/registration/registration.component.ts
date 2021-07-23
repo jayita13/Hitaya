ddcde3c858da5edcf0fd70ef37126344ace340b2
raj713335 <raj713335@gmail.com>
2021-07-24 02:45:50 +0530

    Registration frontend done

--- HitayaApp/src/app/registration/registration.component.ts
+++ HitayaApp/src/app/registration/registration.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
+
+
   constructor() { }
 
   ngOnInit(): void {
+    const sign_in_btn = document.querySelector("#sign-in-btn");
+    const sign_up_btn = document.querySelector("#sign-up-btn");
+    const container = document.querySelector(".container");
+
+    sign_up_btn.addEventListener("click", () => {
+      container.classList.add("sign-up-mode");
+    });
+
+    sign_in_btn.addEventListener("click", () => {
+      container.classList.remove("sign-up-mode");
+    });
+
   }
+
 
 }

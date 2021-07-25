1777695ffeaf3cfeef2de828ed691a390cd0bbc5
raj713335 <raj713335@gmail.com>
2021-07-25 16:00:45 +0530

    Scaffolding Done

---
+++ Hitaya/Hitaya.DAL/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Hitaya.DAL.Models
+{
+    public partial class Employee
+    {
+        public string Empid { get; set; }
+        public string Name { get; set; }
+        public string Emailid { get; set; }
+        public string Department { get; set; }
+        public string Roleid { get; set; }
+    }
+}
---
+++ Hitaya/Hitaya.DAL/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Hitaya.DAL.Models
+{
+    public partial class Transaction
+    {
+        public int TransId { get; set; }
+        public string DebitId { get; set; }
+        public string CreditId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime? SysTrnsDate { get; set; }
+    }
+}

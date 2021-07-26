d583511ce85895fafd43c54f94b35989509208f6
raj713335 <raj713335@gmail.com>
2021-07-26 08:34:42 +0530

    Auth Guard Completed

--- HitayaApp/src/app/hitaya-services/auth-guard/auth-guard.service.ts
+++ HitayaApp/src/app/hitaya-services/auth-guard/auth-guard.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService {
 
-  constructor() { }
+  constructor(private _router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    //check condition
+    if (sessionStorage.getItem('userName') == null) {
+      alert('You are not allowed to view this page');
+      //return false to cancel the navigation
+      this._router.navigate(['/register']);
+      return false;
+    }
+    return true;
+  }
 }

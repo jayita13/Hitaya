43cc9a405f8828ba5b610ac1b6667051e6b74b46
raj713335 <raj713335@gmail.com>
2021-08-07 13:11:57 +0530

    Added Truffle Suite

--- HitayaApp/src/app/wallet/wallet.component.ts
+++ HitayaApp/src/app/wallet/wallet.component.ts
@@ -21,7 +21,24 @@ export class WalletComponent implements OnInit {
   userForm: FormGroup;
   user: any;
 
-  constructor(private router: Router) {
+
+  accountValidationMessages = {
+    transferAddress: [
+      { type: 'required', message: 'Transfer Address is required' },
+      { type: 'minLength', message: 'Transfer Address must be 42 characters long' },
+      { type: 'maxLength', message: 'Transfer Address must be 42 characters long' }
+    ],
+    amount: [
+      { type: 'required', message: 'Amount is required' },
+      { type: 'pattern', message: 'Amount must be a positive number' }
+    ],
+    remarks: [
+      { type: 'required', message: 'Remarks are required' }
+    ]
+  };
+
+  constructor(private fb: FormBuilder, private router: Router) {
+
     this.userName = sessionStorage.getItem('userName');
     console.log(this.userName);
     if (this.userName != null) {
@@ -35,6 +52,44 @@ export class WalletComponent implements OnInit {
 
   ngOnInit(): void {
 
+    this.formSubmitted = false;
+    this.user = { address: '', transferAddress: '', balance: '', amount: '', remarks: '' };
+    this.getAccountAndBalance();
+    this.createForms();
+
   }
 
+
+  createForms() {
+    this.userForm = this.fb.group({
+      transferAddress: new FormControl(this.user.transferAddress, Validators.compose([
+        Validators.required,
+        Validators.minLength(42),
+        Validators.maxLength(42)
+      ])),
+      amount: new FormControl(this.user.amount, Validators.compose([
+        Validators.required,
+        Validators.pattern('^[+]?([.]\\d+|\\d+[.]?\\d*)$')
+      ])),
+      remarks: new FormControl(this.user.remarks, Validators.compose([
+        Validators.required
+      ]))
+    });
+  }
+  getAccountAndBalance = () => {
+    const that = this;
+    that.user.address = '0xd8d0101f83e79fb4e8d21134f5325e64816bd6a0';
+    that.user.balance = 0;
+    // TODO: fetch data
+  }
+  submitForm() {
+    if (this.userForm.invalid) {
+      alert('transfer.components :: submitForm :: Form invalid');
+      return;
+    } else {
+      console.log('transfer.components :: submitForm :: this.userForm.value');
+      console.log(this.userForm.value);
+      // TODO: service call
+    }
+  }
 }

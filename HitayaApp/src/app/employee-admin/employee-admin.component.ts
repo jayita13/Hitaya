50bc240854b8e904bdd573cbb2584a6b15e7a85f
Arnab Das <raj713335@gmail.com>
2021-09-18 03:00:56 +0530

    Added Add Merchant Functionality In Employee Admin

--- HitayaApp/src/app/employee-admin/employee-admin.component.ts
+++ HitayaApp/src/app/employee-admin/employee-admin.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 import { ITransaction } from '../hitaya-interfaces/ITransaction';
+import { IContact } from '../hitaya-interfaces/IContact';
 import { IEmployee } from '../hitaya-interfaces/IEmployee';
 import { HatTokenService } from '../hitaya-services/HAT_TOKEN/hat-token.service';
 
@@ -34,10 +35,21 @@ export class EmployeeAdminComponent implements OnInit {
   commonLayout: boolean = false;
   adminLayout: boolean = true;
 
+  add_contacts: boolean = false;
+
   addemployee: IEmployee;
 
   adminid: any;
   employees: any;
+
+  user: any;
+
+  transfer: ITransaction;
+  contact: IContact;
+  contact_list = [];
+
+
+  
 
   constructor(private router: Router, private hat_token_servie: HatTokenService) {
     this.userName = sessionStorage.getItem('userName');
@@ -54,6 +66,8 @@ export class EmployeeAdminComponent implements OnInit {
 
     this.getAccountAndBalance();
     this.getEmployee();
+    this.getContacts();
+    this.getAccountAndBalance();
 
     this.miniChart_1 = new Chart("mini-chart-1", {
       type: 'line',
@@ -213,6 +227,18 @@ export class EmployeeAdminComponent implements OnInit {
   }
 
 
+  addcontact() {
+    if (this.add_contacts == false) {
+      this.add_contacts = true;
+    }
+    else {
+      this.add_contacts = false;
+    }
+
+  }
+
+
+
   getUserData() {
     alert("Worked");
   
@@ -254,6 +280,36 @@ export class EmployeeAdminComponent implements OnInit {
         console.log(error);
       });
 
+  }
+
+ 
+  
+
+  getContacts = () => {
+    const that = this;
+    this.hat_token_servie.view_contacts().
+      then(function (employee_data: any) {
+        for (var i = 0; i < employee_data.length; i++) {
+          console.log(employee_data);
+          if (that.adminid == employee_data[i][0]) {
+            that.contact_list.push(employee_data[i]);
+          }
+        }
+
+        console.log("IT Worked LA LA");
+      }).catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  SubmitAddContact(form: NgForm) {
+    console.log("Add Contact Fuction Started");
+    this.contact = { owner: this.adminid, contactid: form.value.crypto, name: form.value.name, type: form.value.type };
+    console.log(this.contact);
+    this.hat_token_servie.add_contact(this.contact).
+      then(function () { }).catch(function (error) {
+        console.log(error);
+      });
   }
 
 }

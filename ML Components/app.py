3c550ce207166c57cfc0ecd0144ac7d15cbaf9d6
Arnab Das <raj713335@gmail.com>
2021-10-23 16:15:38 +0530

    Made Changes in App.py File

--- ML Components/app.py
+++ ML Components/app.py
@@ -1,36 +1,23 @@
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
-
-
-from flask import Flask, render_template, request, jsonify,redirect
-
 import easyocr as ocr  #OCR
 from PIL import Image #Image Processing
 import numpy as np #Image Processing
 from io import BytesIO
 from fastapi.middleware.cors import CORSMiddleware
 
+
+from flask import Flask, render_template, request, jsonify,redirect
+
 import tensorflow as tf
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.preprocessing.image import img_to_array
 
-
-
 import pickle
 from health import health
 
 app = FastAPI()
-
-
-json_file = open('Models/Pneumonia/model.json','r')
-loaded_model_json = json_file.read()
-json_file.close()
-global model
-model = model_from_json(loaded_model_json)
-
-#load weights into new model
-model.load_weights("Models/Pneumonia/model.h5")
 
 
 pickle_in = open("Models/classifier.pkl", "rb")
@@ -47,18 +34,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get('/')
-def index():
-    return {'message': 'Hello, This is an API for OCR'}
 
-def read_imagefile(file) -> Image.Image:
-    image = Image.open(BytesIO(file))
-    return image
+def load_model():
+    #open file with model architecture
+    json_file = open('Models/Pneumonia/model.json','r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    global model
+    model = model_from_json(loaded_model_json)
+
+    #load weights into new model
+    model.load_weights("Models/Pneumonia/model.h5")
+    print(model.summary())
 
 
 def process_image(image):
     #read image
-    image = Image.open(BytesIO(image))
+    #image = Image.open(BytesIO(image))
     if image.mode != "RGB":
         image = image.convert("RGB")
 
@@ -70,25 +62,53 @@ def process_image(image):
     return image
 
 
+@app.get('/')
+def index():
+    return {'message': 'Hello, This is an API for OCR'}
+
+
+def read_imagefile(file) -> Image.Image:
+    image = Image.open(BytesIO(file))
+    return image
+
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
     if not extension:
         return "Image must be jpg or png format!"
-
     image = read_imagefile(await file.read())
     image = np.asarray(image)
-
     reader = ocr.Reader(['en'], model_storage_directory='.')
     result = reader.readtext(image)
-
     result_text = []  # empty list for results
-
     for text in result:
         result_text.append(text[1])
 
     return result_text
+
+
+
+@app.post("/pneumonia")
+async def pneumonia(file: UploadFile = File(...)):
+    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    if not extension:
+        return "Image must be jpg or png format!"
+
+    predictions = {}
+    load_model()
+
+    image = read_imagefile(await file.read())
+    image = process_image(image)
+    try:
+        out = model.predict(image)
+        # send the predictions to index page
+        predictions = {"positive":str(np.round(out[0][1],2)),"negative":str(np.round(out[0][0],2))}
+    except:
+        predictions ={}
+
+    return predictions
+
 
 @app.post('/health')
 async def health(data: health):
@@ -102,24 +122,6 @@ async def health(data: health):
     prediction = classifier.predict([[Status, Alcohol, BMI]])
 
     return prediction[0]
-
-
-@app.post('/pneumonia')
-async def pneumonia():
-
-    predictions = {}
-
-    # only make predictions after sucessfully receiving the file
-    if request.files:
-        try:
-            image = request.files["image"].read()
-            image = process_image(image)
-            out = model.predict(image)
-            # send the predictions to index page
-            predictions = {"positive":str(np.round(out[0][1],2)),"negative":str(np.round(out[0][0],2))}
-        except:
-            predictions ={}
-    return predictions
 
 
 

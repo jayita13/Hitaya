be0816e73d9dbcded0ce7f54b7e9c61247704cf1
Arnab Das <raj713335@gmail.com>
2021-12-26 05:58:30 +0530

    Edit User GUI changes

--- HitayaApp/src/app/app.routing.ts
+++ HitayaApp/src/app/app.routing.ts
@@ -44,7 +44,7 @@ const routes: Routes = [
   { path: 'crypto', component: CryptoDataComponent/*, canActivate: [AuthGuardService]*/  },
   { path: 'stocks', component: StockMarketComponent/*, canActivate: [AuthGuardService] */ },
   { path: 'analysis', component: RetirementSavingsComponent/*, canActivate: [AuthGuardService]*/  },
-  { path: 'edit-user', component: EditUserDetailsComponent, canActivate: [AuthGuardService]  },
+  { path: 'edit-user', component: EditUserDetailsComponent/*, canActivate: [AuthGuardService]*/  },
   { path: 'user-dashboard', component: UserDashboardComponent, canActivate: [AuthGuardService]  },
   { path: 'savings-dashboard', component: SavingsDashboardComponent, canActivate: [AuthGuardService]  },
   { path: 'about', component: AboutComponent/*, canActivate: [AuthGuardService]*/ },

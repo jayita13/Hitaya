6f9bba03e0a4e821015250ddc22cb96a95f645bf
Arnab Das <raj713335@gmail.com>
2021-12-26 05:52:57 +0530

    Stock Market GUI Changes

--- HitayaApp/src/app/app.routing.ts
+++ HitayaApp/src/app/app.routing.ts
@@ -42,7 +42,7 @@ const routes: Routes = [
   { path: 'health', component: HealthComponent/*, canActivate: [AuthGuardService]*/ },
   { path: 'doctors', component: DoctorsComponent/*, canActivate: [AuthGuardService]*/ },
   { path: 'crypto', component: CryptoDataComponent/*, canActivate: [AuthGuardService]*/  },
-  { path: 'stocks', component: StockMarketComponent, canActivate: [AuthGuardService]  },
+  { path: 'stocks', component: StockMarketComponent/*, canActivate: [AuthGuardService] */ },
   { path: 'analysis', component: RetirementSavingsComponent, canActivate: [AuthGuardService]  },
   { path: 'edit-user', component: EditUserDetailsComponent, canActivate: [AuthGuardService]  },
   { path: 'user-dashboard', component: UserDashboardComponent, canActivate: [AuthGuardService]  },
